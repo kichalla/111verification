7975c1799dd629bbbbe5a168074bd1a3c1cf1fe8
Kiran Challa <kichalla@microsoft.com>
2017-02-23 13:12:44 -0800

    Updated app to accept command line arguments

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace apps
 {
@@ -12,8 +13,11 @@ namespace apps
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
